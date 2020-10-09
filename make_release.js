10206b3414d56f5c714825b9ec2e365b3ebc6060
Stephen Cefali <scefali@sentry.io>
2020-10-09 14:18:46 -0700

    update

--- make_release.js
+++ make_release.js
@@ -4,30 +4,22 @@ main();
 
 async function main() {
   const cli = new SentryCli().releases;
-  const version = "10.9.2";
+  const version = "10.9.3";
   const prefix = "static/js";
 
   const projects = ["test-sentry-cli-js", "test-steve"];
   await cli.new(version, { projects });
   await cli.setCommits(version, { auto: true });
 
-  const sourceMapOptions = {
-    projects,
-    urlPrefix: `~/${prefix}`,
-    validate: true,
-    include: [`build/${prefix}`],
-  };
-  await cli.uploadSourceMaps(version, sourceMapOptions);
-
-  // await Promise.all(
-  //   projects.map((project) => {
-  //     const sourceMapOptions = {
-  //       projects: [project],
-  //       urlPrefix: `~/${prefix}`,
-  //       validate: true,
-  //       include: [`build/${prefix}`],
-  //     };
-  //     return cli.uploadSourceMaps(version, sourceMapOptions);
-  //   })
-  // );
+  await Promise.all(
+    projects.map((project) => {
+      const sourceMapOptions = {
+        projects: [project],
+        urlPrefix: `~/${prefix}`,
+        validate: true,
+        include: [`build/${prefix}`],
+      };
+      return cli.uploadSourceMaps(version, sourceMapOptions);
+    })
+  );
 }
